8d0c33326293b73915b4a4f1b7c33cdea55c2790
Mina Mansour <mansourmina10@gmail.com>
2024-04-15 01:10:03 +0200

    Dynamic creation of appointments

--- server/db/dataHandler.php
+++ server/db/dataHandler.php
@@ -33,11 +33,34 @@ class DataHandler
     private static function getAppointments()
     {
         $demodata = [
-            [new Appointment(1, "Soccer", "Vienna", " ", "2024-10-02", "2024-10-12", "8", "10")],
-            [new Appointment(2, "Basketball", "Linz", " ", "2024-03-02", "2024-03-07", "10", "11")],
-            [new Appointment(3, "Party", "Berlin", " ", "2024-07-15", "2024-07-20", "15", "17")],
-            [new Appointment(4, "Baseball", "London", " ", "2024-08-02", "2024-08-10", "12", "13")],
-            [new Appointment(5, "Birtday", "New York", " ", "2024-08-10", "2024-08-20", "14", "15")],
+            [new Appointment(1, "Soccer", "Fussball wird gespielt", "Wien", "2024-10-02", "2024-10-12", "8", "10", "Mina", 1, array(
+                (object) [
+                    "name" => "Mina",
+                ],
+                (object) [
+                    "name" => "Bertan"
+                ]
+            ))],
+            [new Appointment(2, "Basketball", "Basketball wird gespielt", "Linz", "2024-03-02", "2024-03-07", "10", "11", "Mina", 2,  array(
+                (object) [
+                    "name" => "Mina"
+                ]
+            ))],
+            [new Appointment(3, "Party", "Party wird gemacht", "Hamburg", "2024-07-15", "2024-07-20", "15", "17", "Bertan", 3,  array(
+                (object) [
+                    "name" => "Mina"
+                ]
+            ))],
+            [new Appointment(4, "Baseball", "Baseball wird gespielt", "Bayern", "2024-08-02", "2024-08-10", "12", "13", "Mina", 3,  array(
+                (object) [
+                    "name" => "Mina"
+                ]
+            ))],
+            [new Appointment(5, "Birtday", "Birtday wird gemacht", "New York", "2024-08-10", "2024-08-20", "14", "15", "Bertan", 3,  array(
+                (object) [
+                    "name" => "Mina"
+                ]
+            ))],
         ];
         return $demodata;
     }
--- server/models/appointments.php
+++ server/models/appointments.php
@@ -9,9 +9,11 @@ class Appointment
   public $expiry_date;
   public $start_date;
   public $end_date;
+  public $organizer;
+  public $duration;
+  public $votes;
 
-
-  function __construct($id, $title, $description, $location, $date, $expiry_date, $start_date, $end_date)
+  function __construct($id, $title, $description, $location, $date, $expiry_date, $start_date, $end_date, $organizer, $duration, $votes)
   {
     $this->id = $id;
     $this->title = $title;
@@ -21,5 +23,8 @@ class Appointment
     $this->expiry_date = $expiry_date;
     $this->start_date = $start_date;
     $this->end_date = $end_date;
+    $this->organizer = $organizer;
+    $this->duration = $duration;
+    $this->votes = $votes;
   }
 }
